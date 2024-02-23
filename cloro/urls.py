8586cb4d775c38bf2b82255cac7259152d405491
aminot27 <ajhonjtc26@gmail.com>
2024-02-22 19:57:06 -0500

    adding requeriments.txt

--- cloro/urls.py
+++ cloro/urls.py
@@ -3,5 +3,5 @@ from cloro.views import CloroApiView, CloroApiViewDetail
 
 urlpatterns_cloro = [
     path('sensor', CloroApiView.as_view()),
-    path('sensor/<int:id', CloroApiViewDetail.as_view()),
+    path('sensor/<int:id>', CloroApiViewDetail.as_view()),
     ]
